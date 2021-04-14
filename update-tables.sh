615694271ac4048c15a9b12a052b70b10457e97f
Lars Buntemeyer <lars.buntemeyer@hzg.de>
2021-04-14 18:46:43 +0200

    Update update-tables.sh

--- update-tables.sh
+++ update-tables.sh
@@ -1,8 +1,8 @@
 echo "Cloning destination git repository"
 # Setup git
 git config --global user.email "lars.buntemeyer@hzg.de"
-git config --global user.name "github action"
-git clone "https://$API_TOKEN_GITHUB@github.com/euro-cordex/tables"
+git config --global user.name "larsbuntemeyer"
+git clone "https://larsbuntemeyer:$API_TOKEN_GITHUB@github.com/euro-cordex/tables"
 ls -la tables
 mv euro-cordex-esgf.* tables/esgf
 cd tables
