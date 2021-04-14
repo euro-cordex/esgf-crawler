6c82c74aab2a0825652aa4774fd5b7228785c140
github action <lars.buntemeyer@hzg.de>
2021-04-14 17:38:53 +0200

    bugfix

--- update-tables.sh
+++ update-tables.sh
@@ -2,5 +2,5 @@ echo "Cloning destination git repository"
 # Setup git
 git config --global user.email "lars.buntemeyer@hzg.de"
 git config --global user.name "github action"
-git clone "https://$API_TOKEN_GITHUB@github.com/euro-cordex/tables
+git clone "https://$API_TOKEN_GITHUB@github.com/euro-cordex/tables"
 ls -la tables
