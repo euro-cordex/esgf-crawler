ffa0681a486999531bbfec0a73cc838342057a6a
github action <lars.buntemeyer@hzg.de>
2021-04-14 18:00:40 +0200

    test updates

--- update-tables.sh
+++ update-tables.sh
@@ -4,3 +4,8 @@ git config --global user.email "lars.buntemeyer@hzg.de"
 git config --global user.name "github action"
 git clone "https://$API_TOKEN_GITHUB@github.com/euro-cordex/tables"
 ls -la tables
+mv euro-cordex-esgf.* tables/esgf
+cd tables
+git status
+git commit -am"update of euro-cordex simulation table"
+git push origin master
