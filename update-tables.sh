1429a7e1c67e9cfd0e86390e3d1c2db8c06b7c09
Lars Buntemeyer <lars.buntemeyer@hzg.de>
2021-04-14 17:36:15 +0200

    update step included

---
+++ update-tables.sh
@@ -0,0 +1,6 @@
+echo "Cloning destination git repository"
+# Setup git
+git config --global user.email "lars.buntemeyer@hzg.de"
+git config --global user.name "github action"
+git clone "https://$API_TOKEN_GITHUB@github.com/euro-cordex/tables
+ls -la tables
